841f87ea4703d8bb5b6ee29be2bed2cfc9d7ea21
Matt Goodmanson <mgoodm167@gmail.com>
2023-12-15 16:37:23 -0600

    created ability to create simple describable page

--- src/structure/describable-page.ts
+++ src/structure/describable-page.ts
@@ -1,1 +1,22 @@
-export default class DescribablePage {}
+import DescribableComponent from '../components/describable-component';
+import { Describer } from '../components/describer';
+
+export default class DescribablePage {
+    private readonly rootComponent: DescribableComponent;
+    private applicationContainer: Element;
+
+    constructor(rootComponent: DescribableComponent) {
+        this.rootComponent = rootComponent;
+        this.applicationContainer = document.body;
+    }
+
+    container(applicationContainer: Element) {
+        this.applicationContainer = applicationContainer;
+        return this;
+    }
+
+    render() {
+        const app = Describer.build(this.rootComponent.compose());
+        this.applicationContainer.append(app);
+    }
+}
---
+++ test/structure/describable-page.test.ts
@@ -0,0 +1,11 @@
+import DescribablePage from '../../src/structure/describable-page';
+import SimpleHtmlComponent from '../components/test-components/simple-html-component';
+
+describe('Describable page module', () => {
+    test('successfully render app in the document body', () => {
+        new DescribablePage(new SimpleHtmlComponent()).render();
+
+        expect(document.body.childNodes.length).toEqual(1);
+        expect(document.body.firstElementChild).toMatchSnapshot();
+    });
+});

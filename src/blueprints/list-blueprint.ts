15e41f2d1fd12d17cfbd4ffa99924d88dd475185
Matt Goodmanson <mgoodm167@gmail.com>
2023-12-19 15:39:27 -0600

    finished adding render logic and tests for list-blueprint

--- src/blueprints/list-blueprint.ts
+++ src/blueprints/list-blueprint.ts
@@ -1,26 +1,111 @@
 import BlueprintComponent from './blueprint-component';
 import { Blueprint } from './blueprint';
 import DynamicProp from './utils/dynamic-prop';
+import ElementRef from './utils/element-ref';
+import BlueprintBuilder from '../structure/blueprint-builder';
+import { compareValues } from '../util/value-util';
 
 type BlueprintMapper<T> = (dataElement: T) => Blueprint;
 type IdMapper<T> = (dataElement: T) => string;
 
 export default class ListBlueprint<T = any> extends BlueprintComponent {
-    private data: DynamicProp<T>;
-    private blueprintMapper: BlueprintMapper<T>;
-    private idMapper: IdMapper<T>;
+    private readonly blueprintMapper: BlueprintMapper<T>;
+    private readonly idMapper: IdMapper<T>;
+    private containerRef = new ElementRef();
+
+    private order: string[];
+    private elementMap: Map<string, HTMLElement>;
 
     constructor(
-        data: DynamicProp<T>,
+        data: DynamicProp<T[]>,
         blueprintMapper: BlueprintMapper<T>,
         idMapper?: IdMapper<T>,
     ) {
         super();
-        this.data = data;
         this.blueprintMapper = blueprintMapper;
-        this.idMapper = idMapper;
+        this.idMapper =
+            idMapper ??
+            ((dataElement) => {
+                // @ts-ignore
+                if (dataElement['id'] == null) {
+                    throw new Error(
+                        `Data elements do not have an id prop. You must pass in a mapping function to produce an id`,
+                    );
+                }
+
+                // @ts-ignore
+                return dataElement['id'];
+            });
+
+        this.order = [];
+        this.elementMap = new Map();
+
+        data.onElementChange(
+            this.containerRef,
+            this.handleElementReady.bind(this),
+        );
+    }
+
+    private handleElementReady(element: HTMLElement) {
+        return (values: T[], prevValues: T[]) => {
+            const safeGetOrder = (index: number) =>
+                index < this.order.length ? this.order[index] : null;
+
+            const updatedOrder = values.map(this.idMapper);
+            const updatedMap = new Map<string, HTMLElement>();
+            const updatedElements: HTMLElement[] = [];
+            const prevValueMap = new Map<string, T>();
+            prevValues.forEach((val) =>
+                prevValueMap.set(this.idMapper(val), val),
+            );
+
+            for (let i = 0; i < updatedOrder.length; i++) {
+                const id = updatedOrder[i];
+                const oldId = safeGetOrder(i);
+                const data = values[i];
+                if (id !== oldId) {
+                    if (this.elementMap.has(id)) {
+                        const updatedElement = compareValues(
+                            data,
+                            prevValueMap.get(id),
+                        )
+                            ? this.elementMap.get(id)
+                            : BlueprintBuilder.build(
+                                  this.blueprintMapper(data),
+                              );
+                        updatedMap.set(id, updatedElement);
+                        updatedElements.push(updatedElement);
+                    } else {
+                        const updatedElement = BlueprintBuilder.build(
+                            this.blueprintMapper(data),
+                        );
+                        updatedMap.set(id, updatedElement);
+                        updatedElements.push(updatedElement);
+                    }
+                } else {
+                    const updatedElement = compareValues(
+                        data,
+                        prevValueMap.get(id),
+                    )
+                        ? this.elementMap.get(id)
+                        : BlueprintBuilder.build(this.blueprintMapper(data));
+                    updatedMap.set(id, updatedElement);
+                    updatedElements.push(updatedElement);
+                }
+            }
+
+            this.order
+                .filter((id) => !updatedMap.has(id))
+                .map((id) => this.elementMap.get(id))
+                .forEach((el) => el.remove());
+            this.order = updatedOrder;
+            this.elementMap = updatedMap;
+
+            element.innerHTML = '';
+            updatedElements.forEach((child) => element.append(child));
+        };
     }
     compose(): Blueprint {
-        return this.fragment();
+        return this.fragment().ref(this.containerRef);
     }
 }
---
+++ test/blueprints/list-blueprint.test.ts
@@ -0,0 +1,151 @@
+import DynamicProp from '../../src/blueprints/utils/dynamic-prop';
+import ListBlueprint from '../../src/blueprints/list-blueprint';
+import { Blueprint } from '../../src/blueprints/blueprint';
+import BlueprintBuilder from '../../src/structure/blueprint-builder';
+import * as crypto from 'crypto';
+
+const createElementSpy = jest.spyOn(document, 'createElement');
+
+function setupList(items: number) {
+    const prop = new DynamicProp(
+        Array(items)
+            .fill(0)
+            .map((_, i) => ({
+                id: `${i + 1}`,
+                value: crypto.randomUUID().toString(),
+            })),
+    );
+    const list = new ListBlueprint(prop, (data) =>
+        new Blueprint('div').id(data.value.toString()),
+    );
+    const container = BlueprintBuilder.build(list.compose());
+
+    return {
+        prop,
+        list,
+        container,
+    };
+}
+
+describe('ListBlueprint', () => {
+    afterEach(() => {
+        document.body.innerHTML = '';
+    });
+
+    describe('growing list', () => {
+        test('nothing rendered when no data is passed in', () => {
+            const { container } = setupList(0);
+
+            expect(createElementSpy).toHaveBeenCalledTimes(1);
+            expect(container.children.length).toEqual(0);
+        });
+
+        test('default value is rendered into list', () => {
+            const { prop, container } = setupList(5);
+
+            expect(createElementSpy).toHaveBeenCalledTimes(6);
+            expect(container.children.length).toEqual(5);
+            prop.currentValue().forEach((el, i) => {
+                expect(createElementSpy).toHaveBeenNthCalledWith(i + 2, 'div');
+                expect(container.children.item(i).id).toEqual(el.value);
+            });
+        });
+
+        test('add values onto end of list', () => {
+            const { prop, container } = setupList(5);
+
+            expect(createElementSpy).toHaveBeenCalledTimes(6);
+            expect(container.children.length).toEqual(5);
+            prop.currentValue().forEach((el, i) => {
+                expect(container.children.item(i).id).toEqual(el.value);
+            });
+
+            createElementSpy.mockClear();
+            prop.set([
+                ...prop.currentValue(),
+                {
+                    id: '6',
+                    value: '100',
+                },
+            ]);
+
+            expect(createElementSpy).toHaveBeenCalledTimes(1);
+            expect(container.children.length).toEqual(6);
+            prop.currentValue().forEach((el, i) => {
+                expect(container.children.item(i).id).toEqual(el.value);
+            });
+        });
+    });
+
+    describe('shrinking list', () => {
+        test('removes all elements from list', () => {
+            const { prop, container } = setupList(5);
+
+            expect(container.children.length).toEqual(5);
+
+            createElementSpy.mockClear();
+            prop.set([]);
+
+            expect(createElementSpy).not.toHaveBeenCalled();
+            expect(container.children.length).toEqual(0);
+        });
+
+        test('removing elements from end of list removes them from the dom', () => {
+            const { prop, container } = setupList(5);
+
+            expect(container.children.length).toEqual(5);
+
+            createElementSpy.mockClear();
+            prop.set([...prop.currentValue()].slice(0, -1));
+
+            expect(createElementSpy).toHaveBeenCalledTimes(0);
+            expect(container.children.length).toEqual(4);
+
+            prop.currentValue().forEach((data, i) => {
+                expect(container.children.item(i).id).toEqual(data.value);
+            });
+        });
+    });
+
+    describe('reorder list', () => {
+        test('reverse list order', () => {
+            const { prop, container } = setupList(5);
+
+            expect(container.children.length).toEqual(5);
+            prop.currentValue().forEach((data, i) => {
+                expect(container.children.item(i).id).toEqual(data.value);
+            });
+
+            createElementSpy.mockClear();
+
+            prop.set(prop.currentValue().reverse());
+
+            expect(createElementSpy).not.toHaveBeenCalled();
+            expect(container.children.length).toEqual(5);
+            prop.currentValue().forEach((data, i) => {
+                expect(container.children.item(i).id).toEqual(data.value);
+            });
+        });
+    });
+
+    describe('update values', () => {
+        test('changing ids re-renders with proper ids', () => {
+            const { prop, container } = setupList(5);
+
+            expect(container.children.length).toEqual(5);
+
+            createElementSpy.mockClear();
+            const updatedValues = prop.currentValue().map((data) => {
+                data.value = crypto.randomUUID().toString();
+                return data;
+            });
+            prop.set(updatedValues);
+
+            expect(createElementSpy).toHaveBeenCalledTimes(5);
+            expect(container.children.length).toEqual(5);
+            updatedValues.forEach((val, index) => {
+                expect(container.children.item(index).id).toEqual(val.value);
+            });
+        });
+    });
+});

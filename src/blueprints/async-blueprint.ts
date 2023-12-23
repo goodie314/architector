cb14af548e84a1eef58c8bbc819600a03fe2401a
Matt Goodmanson <mgoodm167@gmail.com>
2023-12-22 23:30:56 -0600

    setup async blueprints

---
+++ src/blueprints/async-blueprint.ts
@@ -0,0 +1,60 @@
+import BlueprintComponent from './blueprint-component';
+import { Blueprint } from './blueprint';
+import { BlueprintCallback } from '../models/types';
+import ElementRef from './utils/element-ref';
+import BlueprintBuilder from '../structure/blueprint-builder';
+
+export default class AsyncBlueprint extends BlueprintComponent {
+    private promise: Promise<any>;
+    private _waitingView: Blueprint;
+    private _resolvedView: BlueprintCallback;
+    private _rejectedView: BlueprintCallback;
+    private renderRef = new ElementRef();
+
+    constructor(promise: Promise<any>) {
+        super();
+        this.promise = promise;
+        this._waitingView = Blueprint.Fragment();
+    }
+
+    waitingView(blueprint: Blueprint) {
+        this._waitingView = blueprint;
+        return this;
+    }
+
+    resolvedView(callback: BlueprintCallback) {
+        this._resolvedView = callback;
+        return this;
+    }
+
+    rejectedView(callback: BlueprintCallback) {
+        this._rejectedView = callback;
+        return this;
+    }
+
+    compose(): Blueprint {
+        this.promise
+            .then((value) =>
+                this.renderRef.get().then((elem) => {
+                    elem.innerHTML = '';
+                    const updatedView = BlueprintBuilder.build(
+                        this._resolvedView(value),
+                    );
+                    elem.append(updatedView);
+                }),
+            )
+            .catch((err) =>
+                this.renderRef.get().then((elem) => {
+                    if (this._rejectedView) {
+                        elem.innerHTML = '';
+                        const updatedView = BlueprintBuilder.build(
+                            this._rejectedView(err),
+                        );
+                        elem.append(updatedView);
+                    }
+                }),
+            );
+
+        return this._waitingView.ref(this.renderRef);
+    }
+}
---
+++ test/blueprints/async-blueprint.test.ts
@@ -0,0 +1,58 @@
+import BlueprintBuilder from '../../src/structure/blueprint-builder';
+import AsyncBlueprint from '../../src/blueprints/async-blueprint';
+import { Blueprint } from '../../src/blueprints/blueprint';
+
+describe('AsyncBlueprint', () => {
+    describe('waitingView', () => {
+        test('renders whatever is passed in as the waiting view', () => {
+            const promise = new Promise(() => {});
+            const elem = BlueprintBuilder.build(
+                new AsyncBlueprint(promise).waitingView(
+                    new Blueprint('div').id('loading').text('Loading...'),
+                ),
+            );
+
+            expect(elem.id).toEqual('loading');
+            expect(elem.textContent).toEqual('Loading...');
+        });
+    });
+
+    describe('resolvedView', () => {
+        test('displays resolvedView data when promise completes', async () => {
+            jest.useFakeTimers();
+            const promise = Promise.resolve('resolved');
+            const elem = BlueprintBuilder.build(
+                new Blueprint('div')
+                    .id('container')
+                    .append(
+                        new AsyncBlueprint(promise).resolvedView((value) =>
+                            new Blueprint('div').id(value),
+                        ),
+                    ),
+            );
+
+            await jest.runAllTimersAsync();
+            expect(elem.children[0].id).toEqual('resolved');
+        });
+    });
+
+    describe('rejectedView', () => {
+        test('renders rejected view when promise rejects', async () => {
+            jest.useFakeTimers();
+            const promise = Promise.reject('error message');
+            const elem = BlueprintBuilder.build(
+                new Blueprint('div')
+                    .id('container')
+                    .append(
+                        new AsyncBlueprint(promise).rejectedView((err) =>
+                            new Blueprint('div').id('error').text(err),
+                        ),
+                    ),
+            );
+
+            await jest.runAllTimersAsync();
+            expect(elem.children[0].id).toEqual('error');
+            expect(elem.children[0].textContent).toEqual('error message');
+        });
+    });
+});
